1ff64e310af0bc3ecc6451587d5088d294135c47
dchatt <chatterjee.debapriya@gmail.com>
2017-01-28 12:06:28 -0600

    added mkdir for new data dir

--- bin/generator/generate.sh
+++ bin/generator/generate.sh
@@ -5,9 +5,11 @@ echo "#define MAX_DIST 100" >> ../../src/generator/TSPConstructor.h
 
 g++ ../../src/generator/TSPConstructor.cpp -o ../../bin/generator/construct
 
+dirname="../../data/$1c"
+mkdir $dirname
+
 for (( i=0; i < $2; ++i ))
-do
-	dirname="../../data/$1c"
+do	
 	filename="tsp_$1c_instance$i.txt"
 	echo generating $dirname/$filename
 	../../bin/generator/construct $dirname/$filename
